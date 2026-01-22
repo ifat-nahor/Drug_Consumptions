4e4518016f08fd8fc36b3a48d9ac1c652ac15b4a
ifat-nahor <ifat.nahor@live.biu.ac.il>
2026-01-22 15:26:53 +0200

    main update

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-# main.py
-"""from src.data_cleaning import run_data_cleaning_pipeline
-
-def main():
-    df_cleaned, cleaning_report = run_data_cleaning_pipeline()
-
-
-from src.Personality_Based_Drug_Usage_Predictor import (
-    run_personality_based_drug_usage_pipeline
-)
-
-def main():
-    run_personality_based_drug_usage_pipeline()
-    print("all ok")
-
-if __name__ == "__main__":
-    main()"""""
-
+#main.py
 from src.data_cleaning import run_data_cleaning_pipeline
 from src.Personality_Based_Drug_Usage_Predictor import run_personality_based_drug_usage_pipeline
 
